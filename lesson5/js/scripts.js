b026d2a2f2c7b28c23a9c603a510985bf4e13ee4
Lehi Sanchez <sanchez.lehi@gmail.com>
2020-10-16 22:06:03 -0400

    Lesson 5 Updates

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,3 +1,7 @@
+
+// RESPONSIVE NAVIGATION
+// ============================================
+
 let nav = document.getElementById('navigation')
 let menu = document.getElementById('hamburger')
 
@@ -6,8 +10,22 @@ function toggleMenu() {
     menu.classList.toggle('responsive');
 }
 
+// LAST UPDATED
+// ============================================
+
 let date = new Date();
 let modified = document.lastModified;
 
 document.getElementById("copyright-year").innerHTML = date.getFullYear();
 document.getElementById("modified-date").innerHTML  = modified;
+
+
+// NOTIFICATION
+// ============================================
+let navigation = document.getElementById('notification');
+let today = new Date();
+let day = today.getDay();
+
+if (day == 5) {
+    navigation.classList.toggle('show');
+}
