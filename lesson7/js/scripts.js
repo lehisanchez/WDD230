3df08d58192aebd9487bdf24af64cb31c0cd2f58
Lehi Sanchez <sanchez.lehi@gmail.com>
2020-10-31 00:09:53 -0400

    Week 7 Updates

--- lesson7/js/scripts.js
+++ lesson7/js/scripts.js
@@ -1,3 +1,37 @@
+
+// RESPONSIVE NAVIGATION
+// ============================================
+
+let nav = document.getElementById('navigation')
+let menu = document.getElementById('hamburger')
+
+function toggleMenu() {
+    nav.classList.toggle('responsive');
+    menu.classList.toggle('responsive');
+}
+
+// LAST UPDATED
+// ============================================
+
+let date = new Date();
+let modified = document.lastModified;
+
+document.getElementById("copyright-year").innerHTML = date.getFullYear();
+document.getElementById("modified-date").innerHTML  = modified;
+
+
+// NOTIFICATION
+// ============================================
+let navigation = document.getElementById('notification');
+let today = new Date();
+let day = today.getDay();
+
+if (day == 5 && navigation) {
+    navigation.classList.toggle('show');
+}
+
+
+
 // GET IMAGES
 // =====================================================
 let images = document.querySelectorAll("img[data-src]");
