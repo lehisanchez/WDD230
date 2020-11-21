2c8879cdd28bc6c87b525eea897b43780695509c
Lehi Sanchez <sanchez.lehi@gmail.com>
2020-11-21 17:04:08 -0500

    Lesson 10 Updates

--- lesson10/js/home.js
+++ lesson10/js/home.js
@@ -10,67 +10,68 @@ fetch(requestURL)
         for (let i = 0; i < towns.length; i++) {
 
             if (towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven") {
-                            // CREATE CARD ELEMENTS w/ CLASS ATTRIBUTES
-            // ----------------------------------------
-            let townCard = document.createElement('div');
-            townCard.setAttribute('class', 'town-card');
 
-            let townInformation = document.createElement('div');
-            townInformation.setAttribute('class', 'town-card-information');
+                // CREATE CARD ELEMENTS w/ CLASS ATTRIBUTES
+                // ----------------------------------------
+                let townCard = document.createElement('div');
+                townCard.setAttribute('class', 'town-card town-' + towns[i].name.replace(" ", "-").toLowerCase());
 
-            let townName = document.createElement('h3');
-            
-            let townMotto = document.createElement('span');
-            townMotto.setAttribute('class', 'town-motto');
-            
-            let townFounded = document.createElement('span');
-            townFounded.setAttribute('class', 'town-founded');
+                let townInformation = document.createElement('div');
+                townInformation.setAttribute('class', 'town-card-information');
 
-            let townPopulation = document.createElement('span');
-            townPopulation.setAttribute('class', 'town-population');
+                let townName = document.createElement('h3');
 
-            let townRainFall = document.createElement('span');
-            townRainFall.setAttribute('class', 'town-rain-fall');
+                let townMotto = document.createElement('span');
+                townMotto.setAttribute('class', 'town-motto');
 
-            let townImageWrapper = document.createElement('div');
-            townImageWrapper.setAttribute('class', 'town-card-image');
+                let townFounded = document.createElement('span');
+                townFounded.setAttribute('class', 'town-founded');
 
-            let townImage = document.createElement('img');
-            
+                let townPopulation = document.createElement('span');
+                townPopulation.setAttribute('class', 'town-population');
 
+                let townRainFall = document.createElement('span');
+                townRainFall.setAttribute('class', 'town-rain-fall');
 
-            // POPULATE ELEMENTS
-            // ----------------------------------------
-            townName.textContent       = towns[i].name;
-            townMotto.textContent      = towns[i].motto;
-            townFounded.textContent    = 'Year Founded: ' + towns[i].yearFounded;
-            townPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
-            townRainFall.textContent   = 'Annual Rain Fall: ' +towns[i].averageRainfall;
-            townImage.setAttribute('src', 'images/' + towns[i].photo);
+                let townImageWrapper = document.createElement('div');
+                townImageWrapper.setAttribute('class', 'town-card-image');
+
+                let townImage = document.createElement('img');
 
 
 
-            // NEST ELEMENTS
-            // ----------------------------------------
-            townInformation.appendChild(townName);
-            townInformation.appendChild(townMotto);
-            townInformation.appendChild(townFounded);
-            townInformation.appendChild(townPopulation);
-            townInformation.appendChild(townRainFall);
-            townImageWrapper.appendChild(townImage);
-            
-
-
-            // NEST PARENT WRAPPERS
-            // ---------------------------------------- 
-            townCard.appendChild(townInformation);
-            townCard.appendChild(townImageWrapper);
+                // POPULATE ELEMENTS
+                // ----------------------------------------
+                townName.textContent = towns[i].name;
+                townMotto.textContent = towns[i].motto;
+                townFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                townPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
+                townRainFall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+                townImage.setAttribute('src', 'images/' + towns[i].photo);
 
 
 
-            // APPEND TO HTML
-            // ----------------------------------------
-            document.querySelector('div.town-cards').appendChild(townCard);
+                // NEST ELEMENTS
+                // ----------------------------------------
+                townInformation.appendChild(townName);
+                townInformation.appendChild(townMotto);
+                townInformation.appendChild(townFounded);
+                townInformation.appendChild(townPopulation);
+                townInformation.appendChild(townRainFall);
+                townImageWrapper.appendChild(townImage);
+
+
+
+                // NEST PARENT WRAPPERS
+                // ----------------------------------------
+                townCard.appendChild(townInformation);
+                townCard.appendChild(townImageWrapper);
+
+
+
+                // APPEND TO HTML
+                // ----------------------------------------
+                document.querySelector('div.town-cards').appendChild(townCard);
             }
 
 
