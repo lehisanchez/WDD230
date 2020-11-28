6893a03662b46482c5f1bfb20c25d85d03ee95b2
Lehi Sanchez <sanchez.lehi@gmail.com>
2020-11-28 04:43:17 -0500

    Lesson 11 Updates

--- lesson11/js/scripts.js
+++ lesson11/js/scripts.js
@@ -83,6 +83,7 @@ function fetchForecast(url) {
                     // POPULATE ELEMENTS
                     day_name.innerHTML = DAYS_OF_THE_WEEK[day];
                     day_icon.setAttribute('src', imagesrc);
+                    day_icon.setAttribute('alt', 'weather icon');
                     day_temp.innerHTML = Math.round(jsObject.list[index].main.temp) + '&deg; F';
 
                     // START PACKING EVERYTHING
@@ -140,70 +141,6 @@ function getTownEvents(city) {
                         document.getElementById('events').appendChild(eventCard);
                     }
 
-
-
-                    // CREATE CARD ELEMENTS w/ CLASS ATTRIBUTES
-                    // ----------------------------------------
-                    // let townCard = document.createElement('div');
-                    // townCard.setAttribute('class', 'town-card town-' + towns[i].name.replace(" ", "-").toLowerCase());
-
-                    // let townInformation = document.createElement('div');
-                    // townInformation.setAttribute('class', 'town-card-information');
-
-                    // let townName = document.createElement('h3');
-
-                    // let townMotto = document.createElement('span');
-                    // townMotto.setAttribute('class', 'town-motto');
-
-                    // let townFounded = document.createElement('span');
-                    // townFounded.setAttribute('class', 'town-founded');
-
-                    // let townPopulation = document.createElement('span');
-                    // townPopulation.setAttribute('class', 'town-population');
-
-                    // let townRainFall = document.createElement('span');
-                    // townRainFall.setAttribute('class', 'town-rain-fall');
-
-                    // let townImageWrapper = document.createElement('div');
-                    // townImageWrapper.setAttribute('class', 'town-card-image');
-
-                    // let townImage = document.createElement('img');
-
-
-
-                    // POPULATE ELEMENTS
-                    // ----------------------------------------
-                    // townName.textContent = towns[i].name;
-                    // townMotto.textContent = towns[i].motto;
-                    // townFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-                    // townPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
-                    // townRainFall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-                    // townImage.setAttribute('src', 'images/' + towns[i].photo);
-                    // townImage.setAttribute('alt', 'Scenic view of ' + towns[i].name);
-
-
-
-                    // NEST ELEMENTS
-                    // ----------------------------------------
-                    // townInformation.appendChild(townName);
-                    // townInformation.appendChild(townMotto);
-                    // townInformation.appendChild(townFounded);
-                    // townInformation.appendChild(townPopulation);
-                    // townInformation.appendChild(townRainFall);
-                    // townImageWrapper.appendChild(townImage);
-
-
-
-                    // NEST PARENT WRAPPERS
-                    // ----------------------------------------
-                    // townCard.appendChild(townInformation);
-                    // townCard.appendChild(townImageWrapper);
-
-
-
-                    // APPEND TO HTML
-                    // ----------------------------------------
-                    // document.querySelector('div.town-cards').appendChild(townCard);
                 }
 
 
