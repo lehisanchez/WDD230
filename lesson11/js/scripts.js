0bc3cde35da2dff85796a136787455b961b66d16
Lehi Sanchez <sanchez.lehi@gmail.com>
2020-11-28 03:39:03 -0500

    Lesson 11 Updates

--- lesson11/js/scripts.js
+++ lesson11/js/scripts.js
@@ -117,6 +117,100 @@ function updateFooter() {
     document.getElementById("modified-date").innerHTML = modified;
 }
 
+function getTownEvents(city) {
+    let url = "https://byui-cit230.github.io/weather/data/towndata.json";
+    fetch(url)
+        .then((response) => response.json())
+        .then((jsonObject) => {
+
+            const towns = jsonObject['towns'];
+            for (let i = 0; i < towns.length; i++) {
+
+                if (towns[i].name == city) {
+
+                    for (let e = 0; e < towns[i].events.length; e++) {
+
+
+                        console.log(towns[i].events[e])
+
+                        let eventCard = document.createElement('div');
+                        eventCard.setAttribute('class', 'event-card');
+
+                        eventCard.textContent = towns[i].events[e];
+                        document.getElementById('events').appendChild(eventCard);
+                    }
+
+
+
+                    // CREATE CARD ELEMENTS w/ CLASS ATTRIBUTES
+                    // ----------------------------------------
+                    // let townCard = document.createElement('div');
+                    // townCard.setAttribute('class', 'town-card town-' + towns[i].name.replace(" ", "-").toLowerCase());
+
+                    // let townInformation = document.createElement('div');
+                    // townInformation.setAttribute('class', 'town-card-information');
+
+                    // let townName = document.createElement('h3');
+
+                    // let townMotto = document.createElement('span');
+                    // townMotto.setAttribute('class', 'town-motto');
+
+                    // let townFounded = document.createElement('span');
+                    // townFounded.setAttribute('class', 'town-founded');
+
+                    // let townPopulation = document.createElement('span');
+                    // townPopulation.setAttribute('class', 'town-population');
+
+                    // let townRainFall = document.createElement('span');
+                    // townRainFall.setAttribute('class', 'town-rain-fall');
+
+                    // let townImageWrapper = document.createElement('div');
+                    // townImageWrapper.setAttribute('class', 'town-card-image');
+
+                    // let townImage = document.createElement('img');
+
+
+
+                    // POPULATE ELEMENTS
+                    // ----------------------------------------
+                    // townName.textContent = towns[i].name;
+                    // townMotto.textContent = towns[i].motto;
+                    // townFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                    // townPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
+                    // townRainFall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+                    // townImage.setAttribute('src', 'images/' + towns[i].photo);
+                    // townImage.setAttribute('alt', 'Scenic view of ' + towns[i].name);
+
+
+
+                    // NEST ELEMENTS
+                    // ----------------------------------------
+                    // townInformation.appendChild(townName);
+                    // townInformation.appendChild(townMotto);
+                    // townInformation.appendChild(townFounded);
+                    // townInformation.appendChild(townPopulation);
+                    // townInformation.appendChild(townRainFall);
+                    // townImageWrapper.appendChild(townImage);
+
+
+
+                    // NEST PARENT WRAPPERS
+                    // ----------------------------------------
+                    // townCard.appendChild(townInformation);
+                    // townCard.appendChild(townImageWrapper);
+
+
+
+                    // APPEND TO HTML
+                    // ----------------------------------------
+                    // document.querySelector('div.town-cards').appendChild(townCard);
+                }
+
+
+            }
+        });
+}
+
 function getTownCards() {
     let url = "https://byui-cit230.github.io/weather/data/towndata.json";
     fetch(url)
@@ -165,6 +259,7 @@ function getTownCards() {
                     townPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
                     townRainFall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
                     townImage.setAttribute('src', 'images/' + towns[i].photo);
+                    townImage.setAttribute('alt', 'Scenic view of ' + towns[i].name);
 
 
 
